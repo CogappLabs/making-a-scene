480a3566bc85f174b8436a275f949c448b407ce5
Neil Hawkins <neilh@cogapp.com>
2024-03-22 09:43:52 +0000

    Add va

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from './components/providers';
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Providers>
           {children}
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
