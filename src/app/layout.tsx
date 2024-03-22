65584a313127a17f0d1ceef01085380cb18ff68f
Neil Hawkins <neilh@cogapp.com>
2024-03-22 21:36:18 +0000

    Ditch providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Providers } from './components/providers';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,10 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`h-screen ${inter.className}`}>
-        {/* Wrapping the children prop */}
-        <Providers>
           {children}
-        </Providers>
         <Analytics />
       </body>
     </html>
